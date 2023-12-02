71d5d03d18c8bf03287aab61d54dd0cac00aeac8
Jihoon_Song <69960711+HelloTech69@users.noreply.github.com>
2023-12-02 23:05:39 +0800

    chore(auth-service): Added interface for JWT Token payload

--- authentication/src/middlewares/JWT/interfaces/index.ts
+++ authentication/src/middlewares/JWT/interfaces/index.ts
@@ -2,4 +2,5 @@ export { AccessClaims } from './AccessClaims';
 export { BaseClaims } from './BaseClaims';
 export { IdentityClaims } from './IdentityClaims';
 export { RefreshClaims } from './RefreshClaims';
+export { TokenPayload } from './TokenPayload';
 export { TokenType } from './TokenType';
