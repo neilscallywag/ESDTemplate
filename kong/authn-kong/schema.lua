37c1c7fc83d2001eb95b3218971b6ab3e9ad463b
neilscallywag <neil.sharma.2022@scis.smu.edu.sg>
2023-12-03 20:12:26 +0800

    feat(Kong-Plugin): Made fixes to kong plugin. To test it out.

--- kong/authn-kong/schema.lua
+++ kong/authn-kong/schema.lua
@@ -1,19 +1,13 @@
-local typedefs = require "kong.db.schema.typedefs"
-
 return {
   name = "authn-kong",
   fields = {
-    { consumer = typedefs.no_consumer },
-    {
-      config = {
-        type = "record",
-        fields = {
-            -- the default_secret needs to be changed with actual secret. 
-            -- preferabbly populated using a env or vault manager
-            -- to discuss
-          { jwt_secret = { type = "string", required = true, default = "default_secret" }, },
-        },
-      },
-    },
+      { config = {
+          type = "record",
+          fields = {
+              { jwt_secret = { type = "string", required = true } },
+              { refresh_endpoint = { type = "string", required = true } },
+              { unauthenticated_endpoints = { type = "array", default = {}, elements = { type = "string" } } },
+          },
+      }},
   },
 }
