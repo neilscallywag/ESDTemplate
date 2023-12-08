1aaaee280f26277a543269ed66ddc17d60c8b5cc
Jihoon_Song <69960711+HelloTech69@users.noreply.github.com>
2023-12-08 16:06:17 +0800

    fix(auth-service): Fixed export and import for the type 'Role'

--- authentication/src/entity/index.ts
+++ authentication/src/entity/index.ts
@@ -2,4 +2,4 @@ export { User } from './User';
 export { UserAuth, UserData } from './UserAuth';
 export { UserDevice, UserDeviceData } from './UserDevice';
 export { UserLocation, UserLocationData } from './UserLocation';
-export { Role, UserRole } from './UserRole';
+export { UserRole } from './UserRole';
