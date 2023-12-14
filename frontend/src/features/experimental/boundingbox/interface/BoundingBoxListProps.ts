7fa60a660707d9f44de6167fc70ac6fae9c1124c
neilscallywag <neil.sharma.2022@scis.smu.edu.sg>
2023-12-14 15:58:18 +0800

    fix(eslinter): configured eslinter to add end of line sequence based on operating system

--- frontend/src/features/experimental/boundingbox/interface/BoundingBoxListProps.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-import ImageManager from "../ImageManager";
-
-export interface BoundingBoxListProps {
-  imageManager: ImageManager;
-  setImageManager: React.Dispatch<React.SetStateAction<ImageManager>>;
-}
