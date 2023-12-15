c5a99edaa93afd803eac88148cffae6676283a19
Jihoon_Song <69960711+HelloTech69@users.noreply.github.com>
2023-12-15 23:13:53 +0800

    feat(client): added encrypt storage for zustand store for security

--- client/src/shared/store/AuthStore.tsx
+++ client/src/shared/store/AuthStore.tsx
@@ -1,7 +1,42 @@
+import { EncryptStorage } from "encrypt-storage";
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
-import { AuthStateType } from "~types";
+import { AuthStateType, ZustandStorage } from "~types";
+
+const STORAGE_KEY = import.meta.env.VITE_STORAGE_KEY;
+
+const encryptedStorage = new EncryptStorage(STORAGE_KEY, {
+  storageType: "sessionStorage",
+  stateManagementUse: true,
+});
+
+const customSessionStorage: ZustandStorage = {
+  getItem: async (key) => {
+    const item = await encryptedStorage.getItem(key);
+    return item !== undefined ? item : null;
+  },
+  setItem: (key, value) => {
+    return new Promise((resolve, reject) => {
+      try {
+        encryptedStorage.setItem(key, value);
+        resolve();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  },
+  removeItem: (key) => {
+    return new Promise((resolve, reject) => {
+      try {
+        encryptedStorage.removeItem(key);
+        resolve();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  },
+};
 
 const useAuthStore = create<AuthStateType>()(
   persist(
@@ -26,7 +61,7 @@ const useAuthStore = create<AuthStateType>()(
     }),
     {
       name: "auth-storage",
-      storage: createJSONStorage(() => sessionStorage),
+      storage: createJSONStorage(() => customSessionStorage),
     },
   ),
 );
--- client/src/shared/types/index.ts
+++ client/src/shared/types/index.ts
@@ -2,3 +2,4 @@ export * from "./AuthContextType";
 export * from "./AuthStateType";
 export * from "./UserData";
 export * from "./UserRole";
+export * from "./ZustandStorage";
