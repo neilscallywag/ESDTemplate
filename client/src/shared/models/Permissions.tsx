0bf30e6b7e86c530d30bfb9ecf42830cccae07f8
Jihoon_Song <69960711+HelloTech69@users.noreply.github.com>
2023-12-17 12:15:42 +0800

    chore(docs): updated sequence diagram and added comments for clarification

--- client/src/shared/models/Permissions.tsx
+++ client/src/shared/models/Permissions.tsx
@@ -7,6 +7,12 @@ const applicationPermissions: PermissionsModel = {
   gayneil: ["org.admin", "org.permissions.SERVICENAME.read"],
 
   /*
+   * Use org.admin or org.user when specific permissions are not required
+   * If this feels unnecessary, just use org.permissions for everything else
+   * However, org.admin should have some permissions by default which dont need to be specified
+   */
+
+  /*
    * Below are examples of permissions that can be used in the application
    * settings: ['org.admin', 'org.permissions.settings.manage'],
    * userManagement: ['org.admin', 'org.permissions.user.manage'],
